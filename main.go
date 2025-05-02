1b2e35b71f79f7c895d61c9f9cb2ac2526a52aca
davidfung <davidfung@amgcomputing.com>
2025-05-01 20:41:34 -0700

    Add skeletal routes

--- main.go
+++ main.go
@@ -1,20 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"net"
+	"net/http"
+	"time"
 )
 
-func main() {
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		fmt.Println(err)
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
 	}
+	t := time.Now()
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(t.Format(time.RFC3339)))
+}
 
-	for _, addr := range addrs {
-		ipNet, ok := addr.(*net.IPNet)
-		if ok && !ipNet.IP.IsLoopback() && ipNet.IP.To4() != nil {
-			fmt.Println(ipNet)
+func home(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Home"))
+}
+
+func menu(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Menu"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/menu", menu)
+	mux.HandleFunc("/time", timeHandler)
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		if err != http.ErrServerClosed {
+			panic(err)
 		}
 	}
 }
